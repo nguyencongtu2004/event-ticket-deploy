c49b6a69820612f79473e9aab5100a35fee296ba
Tú <22521591@gm.uit.edu.vn>
2025-01-13 21:44:32 +0700

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "743946264"
+    serviceWorkerVersion: "517547760"
   }
 });
